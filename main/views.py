215d07f986de5ecb50195a5c6f3d806eda160ccc
Chaos-Loki <chaoskingloki@tutanota.com>
2023-10-18 22:46:45 -0500

    Ultimos retoques

--- main/views.py
+++ main/views.py
@@ -296,3 +296,11 @@ class IndexView(generic.TemplateView):
             context = super().get_context_data(**kwargs)
             return context
         
+        
+class HomeView(generic.TemplateView):
+        template_name = "main/home.html"
+
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            return context
+        
